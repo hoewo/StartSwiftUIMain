906f5b5f95edf2476e0e9cd072e69ff29e92dfa7
BB-Y <356071707@qq.com>
2025-05-17 14:33:42 +0800

    Update create_package.sh

--- create_package.sh
+++ create_package.sh
@@ -124,7 +124,7 @@ EOF
 echo "✅ $PACKAGE_NAME Swift Package 初始化完成！"
 
 # 自动用 Cursor 打开生成的 workspace 文件
-# cursor "../${PROJECT_NAME}.code-workspace"
+cursor "../${PROJECT_NAME}.code-workspace"
 
 # 自动创建并推送 GitHub 远程仓库
 cd ..
