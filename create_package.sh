fe46ea4faa95361232bf90e39371bf94fb4f543d
BB-Y <356071707@qq.com>
2025-05-17 14:39:03 +0800

    Update create_package.sh

--- create_package.sh
+++ create_package.sh
@@ -121,14 +121,15 @@ cat > ../${PROJECT_NAME}.code-workspace <<EOF
 }
 EOF
 
+cd ..
 echo "✅ $PACKAGE_NAME Swift Package 初始化完成！"
 
 # 自动用 Cursor 打开生成的 workspace 文件
 cursor "../${PROJECT_NAME}.code-workspace"
 
 # 自动创建并推送 GitHub 远程仓库
-cd ..
-bash create_github_repo.sh
 
+# bash create_github_repo.sh
+# curl -fsSL "https://raw.githubusercontent.com/hoewo/StartSwiftUIMain/refs/heads/main/create_github_repo.sh | bash
 
 
